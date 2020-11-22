ca7f3df594c30a76ba841dd1e5f325e2504765a5
Jasper Hajonides van der Meulen <38157436+jasperhajonides@users.noreply.github.com>
2020-11-22 15:41:22 +0000

    updated main script

--- UP_Main_script.sh
+++ UP_Main_script.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-# This script calls subscripts for fMRI data preprocessing 
-
+# This script calls scripts for fMRI data preprocessing 
 # Hajonides 11/2020
 
 
@@ -11,30 +10,29 @@ script_path=$rootdir/scripts/preprocessing_pipeline_fmri
 
 # subjects and runs to loop over
 subjects=(sub-02) 
-runs=(unprocessed run-01 run-02 run-03 run-04 run-05 run-06 run-07 run-08) #run-01 run-02 run-03 run-04 run-05 run-06 run-07 run-08) 
+runs=(unprocessed run-01 run-02 run-03 run-04 run-05 run-06 run-07 run-08)
 
 
 #loop over subjects
 for ((s = 0; s < ${#subjects[@]}; s++ )) do 
     #define subject specific variables
     subj=${subjects[$s]}
-    # subj_path=$rootdir/$subj
     
     #preprocessing, taking the data from the scanner and doing basic preprocessing on functional, anatomical, and fieldmap.
-    # source  $script_path/UP_prep_T1.sh $subj
+    source  $script_path/UP_prep_T1.sh $subj
 
     #registration functional data
-    # source $script_path/UP_registration.sh $subj $runs
+    source $script_path/UP_registration.sh $subj $runs
 
     # #(sub)cortical mask prep
-    # source $script_path/UP_basal_ganglia.sh $subj
-    # source $script_path/UP_pulvinar.sh $subj
+    source $script_path/UP_basal_ganglia.sh $subj
+    source $script_path/UP_pulvinar.sh $subj
 
     # Task specific preprocessing
     # - delineated pulvinar + LGN masks
     # - either FIX trained or hand classified txt file
     # - event timings in ./behavioural/SUBJECT/....txt in three column format. 
-    # source $script_path/UP_task_specific.sh $subj $runs
+    source $script_path/UP_task_specific.sh $subj $runs
 
     # higher level
     source $script_path/UP_higher_level.sh $subj $runs
